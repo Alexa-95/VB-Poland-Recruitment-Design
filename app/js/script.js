ea8588fda08f42bd77b533de3c4a663e8c6d88b6
Ola Sobanska <aleksandra.sobanska@kingapp.pl>
2022-03-09 14:28:41 +0100

    header styles

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,30 @@ console.log("Load script.js");
 
 // Instantiating the global app object
 var app = {};
+
+
+//HAMBURGER MENU
+document.getElementById('hide-show_menu').addEventListener('click', function(){
+  this.classList.toggle("active");
+});
+
+//TU DODAĆ ONLOAD
+//check window width
+let windowSize = function(){
+  if(window.innerWidth < 760){
+    document.getElementById('hide-show_menu').style.display = 'inline-flex';
+    // document.getElementsByClassName('nav_conent-menu')[0].classList.add('open');
+    [...document.getElementsByClassName('nav_conent-menu')].forEach(function(e){
+      return e.classList.add('menu-toggle');
+    });
+  }
+  else{
+    document.getElementById('hide-show_menu').style.display = 'none';
+    [...document.getElementsByClassName('nav_conent-menu')].forEach(function(e){
+      return e.classList.remove('menu-toggle');
+    });
+  }
+}
+
+//show/hide hamburger menu on resize 
+window.addEventListener("resize", windowSize());

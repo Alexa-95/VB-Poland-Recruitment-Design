52f896aeebb8f8e492b7615afa58862ade81996f
Aleksandra Sobaska <Aleksandra.Makowska@grupawp.pl>
2022-03-16 21:47:54 +0100

    xhttp response

--- app/js/_request.js
+++ app/js/_request.js
@@ -1,30 +1,14 @@
-// loadDoc = (value) => {
-//   let target = value.path[0].value;
-//   const xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       document.getElementById("main_content").innerHTML =
-//       this.responseText;
-//     }
-//   };
-//   xhttp.open("GET", `/${target}.html`, true);
-//   xhttp.send();
 
-//   window.history.pushState("", "", `${target.split("/").pop()}`);
-//   app.setBodyClass();
-// }
-
-
-// document.getElementById('link').addEventListener('click', function(e) {
-//   e.preventDefault();
-
-//   var httpRequest = new XMLHttpRequest()
-//   httpRequest.onreadystatechange = function () {
-//     /* Kod wykonywany w przypadku powodzenia */
-//     document.getElementById('content').innerHTML = this.responseText;
-//     console.log(this);
-//     history.pushState({foo:'bar'}, 'second page', 'page.html');
-//   }
-//   httpRequest.open('GET', this.href);
-//   httpRequest.send();
-// });
+loadDoc = (path) =>{
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("main_content").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", path, true);
+  xhttp.send();
+  // window.history.pushState("", "", path);
+  collection;
+}
--- app/js/script.js
+++ app/js/script.js
@@ -2,10 +2,12 @@
 
 // Instantiating the global app object
 class App {
-  constructor({dir, hamburgerButton, redirectButton}){
+  constructor({dir, hamburgerButton, userButton, redirectButton, navbar}){
     this.dir = dir;
     this.hamburgerButton = hamburgerButton;
+    this.userButton = userButton;
     this.redirectButton = redirectButton;
+    this.navbar = navbar;
   }
 
   //open hamburger menu, toggle class active
@@ -28,25 +30,20 @@ class App {
     .join(" ").split('.').slice(0, -1).join('.')
     ;
 
-    [...document.getElementsByClassName('navigation-bar')].forEach(function(e){
+    [...this.navbar].forEach(function(e){
       e.innerHTML = `<a href="/">HOME</a> ${newPath}`; 
     });    
   }
 
   //get class name by path
-  setBodyClass = (value) => {
+  setBodyClass = () => {
     document.body.removeAttribute("class");
-    if(value){
-      document.body.classList.add(value.split('/').pop())
+    if(this.dir == '/'){
+      document.body.classList.add('main');
     }
     else{
-      if(this.dir == '/'){
-        document.body.classList.add('main');
-      }
-      else{
-        let path = this.dir.split("/").pop().split(".").shift();
-        document.body.classList.add(path);
-      }
+      let path = this.dir.split("/").pop().split(".").shift();
+      document.body.classList.add(path);
     }
   }
 
@@ -66,20 +63,21 @@ class App {
     }
   };
 
-  loadDoc = (value) => {
-    let target = value.path[0].value;
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("main_content").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("GET", `/${target}.html`, true);
-    xhttp.send();
-  
-    window.history.pushState("", "", `${target.split("/").pop()}`);
-  };
+  loadPage(e) {
+    e.preventDefault();
+    let target = this.getAttribute('value');
+    let path = this.pathname;
+
+    //load html
+    loadDoc(path);
+
+    //load collection
+    collection();
+
+    //add class to body
+    document.body.removeAttribute("class");
+    document.body.classList.add(target);
+  }
 
   run(){
     this.getPathname();
@@ -88,32 +86,26 @@ class App {
 
     //event listeners
     window.addEventListener("resize", this.windowSize);
-    document.getElementById('hide-show_menu').addEventListener('click', this.toggleClassActive);
+    this.hamburgerButton.addEventListener('click', this.toggleClassActive);
 
-    Array.from(this.redirectButton).forEach((element) => {
-      element.addEventListener('click', function(e) {
-        e.preventDefault();
-        console.log(this.pathname)
-        let target = this.pathname;
-      
-        var httpRequest = new XMLHttpRequest()
-        httpRequest.onreadystatechange = function () {
-          document.getElementById("main_content").innerHTML = this.responseText;
-          console.log(this);
-          history.pushState({foo:'bar'}, target, target);
-        }
-        httpRequest.open('GET', this.href);
-        httpRequest.send();
-      });
+    Array.from(this.userButton).forEach((element) => {
+      element.addEventListener('click', this.toggleClassActive);
     });
 
+    Array.from(this.redirectButton).forEach((element) => {
+      // element.addEventListener('click', collection);
+      element.addEventListener('click', this.loadPage);
+    });
+    window.onload = collection;
   }
 };
 
 const app = new App({
   dir: window.location.pathname,
   hamburgerButton: document.getElementById('hide-show_menu'),
-  redirectButton: document.getElementsByClassName('redirectCollection')
+  userButton: document.getElementsByClassName('hide-show_userpanel'),
+  redirectButton: document.getElementsByClassName('redirect'),
+  navbar: document.getElementsByClassName('navigation-bar')
 });
 
 app.run();
